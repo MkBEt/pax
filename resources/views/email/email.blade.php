9a333d95ad56bcaab8c35c1f8836b4e2f50adfd4
Milan Amatya <amatyamilan@hotmail.com>
2018-06-20 07:22:12 +0545

    test

--- resources/views/email/email.blade.php
+++ resources/views/email/email.blade.php
@@ -1,2 +1,2 @@
 <h1>Click the Link To Verify Your Email</h1>
-Click the following link to verify your email <a> {{url('/verifyemail/'.$email_token)}}</a>
+Click the following here to verify your email <a href="{{url('/verifyemail/'.$email_token)}}">FiaSteem Login</a>
