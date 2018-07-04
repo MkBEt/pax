32173bd46e89adb73069854b7b0ca85393101fb0
Milan Amatya <amatyamilan@hotmail.com>
2018-07-04 23:33:16 +0545

    contact bar update

--- resources/views/frontend/master_frontend.blade.php
+++ resources/views/frontend/master_frontend.blade.php
@@ -6,6 +6,7 @@
 	@yield('metaTags')
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<meta name="theme-color" content="#50d8af">
 
 
 	<!-- Favicons -->
